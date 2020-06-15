672127cb7c09f0ce40a6d77f045f32ca7625064d
jngyi.zhang <jingyi.zhang@rograndec.com>
2020-06-15 09:15:10 +0800

    修改页面-张静怡-2020年06月14日

--- src/main/java/com/imooc/web/controller/AppErrorController.java
+++ src/main/java/com/imooc/web/controller/AppErrorController.java
@@ -1,17 +1,22 @@
 package com.imooc.web.controller;
 
+import com.imooc.base.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 public class AppErrorController implements ErrorController {
-    private static final String ERROR_PATH ="/error";
+    private static final String ERROR_PATH = "/error";
 
     private ErrorAttributes errorAttributes;
 
@@ -31,7 +36,7 @@ public class AppErrorController implements ErrorController {
      * @param response
      * @return
      */
-    @RequestMapping(value = ERROR_PATH,produces = "text/html")
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
     public String errorPageHandler(HttpServletRequest request, HttpServletResponse response){
         int status = response.getStatus();
         switch (status){
@@ -44,4 +49,27 @@ public class AppErrorController implements ErrorController {
         }
         return "index";
     }
+    /**
+     * 除Web页面外的错误处理，比如Json/XML等
+     */
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ApiResponse errorApiHandler(HttpServletRequest request) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+
+        Map<String, Object> attr = this.errorAttributes.getErrorAttributes(requestAttributes, false);
+        int status = getStatus(request);
+
+        return ApiResponse.ofMessage(status, String.valueOf(attr.getOrDefault("message", "error")));
+    }
+
+    private int getStatus(HttpServletRequest request) {
+        Integer status = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (status != null) {
+            return status;
+        }
+
+        return 500;
+    }
+
 }
--- src/main/java/com/imooc/web/controller/HomeController.java
+++ src/main/java/com/imooc/web/controller/HomeController.java
@@ -15,9 +15,23 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/get")
-    @ResponseBody
-    public ApiResponse get(){
-        return new ApiResponse(200,"成功了！",null);
+    @GetMapping("/404")
+    public String notFoundPage(){
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError(){
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError(){
+        return "500";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage(){
+        return "logout";
     }
 }
